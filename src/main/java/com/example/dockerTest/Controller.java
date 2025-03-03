41af994f062b34de9589d92dcd6c83c98f1ce02d
Kaouthar Essaheli <jk_essaheli@esi.dz>
2025-03-03 15:08:37 +0000

    App to test jenkins with deploy in docker

---
+++ src/main/java/com/example/dockerTest/Controller.java
@@ -0,0 +1,14 @@
+package com.example.dockerTest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/test")
+    String getTest()
+    {
+        return "Hello from test controller in this java spring app";
+    }
+}
+
